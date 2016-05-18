70fe1a4a5458b4ed9052a130b3694c0b1f15b9d3
孙传俊 <chjsun@chjsundemacbook-air.local>
2016-05-19 00:21:20 +0800

    Initial Commit

---
+++ instant/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  instant
+//
+//  Created by chjsun on 16/5/19.
+//  Copyright © 2016年 chjsun. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
